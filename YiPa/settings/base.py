521c809ef325a5deb6dad4b6704ee9409f99f263
rapospectre <rapospectre@163.com>
2015-09-28 17:49:37 +0800

    add static file

--- YiPa/settings/base.py
+++ YiPa/settings/base.py
@@ -49,9 +49,6 @@ WSGI_APPLICATION = 'YiPa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-
-
-
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
 )
@@ -73,3 +70,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = './static/'
